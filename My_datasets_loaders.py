d6e6cd1c68bb632cebd59d29de675ff0a9d080c1
E Magnan <34895417+Magnaner@users.noreply.github.com>
2018-02-05 19:51:50 -0500

    Update My_datasets_loaders.py

--- My_datasets_loaders.py
+++ My_datasets_loaders.py
@@ -208,11 +208,11 @@ def plot_decision_boundary(model, X, y):
 #############################
     
 def load_cats_images():
-    train_dataset = h5py.File('Datasets/Cats/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('Cats/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/Cats/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File('Cats/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
